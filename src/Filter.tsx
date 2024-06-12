f7f3cc8a8cbb8f58cd7eec6086b7a2748647550a
mtopgul <muhammedtopgul.29@gmail.com>
2024-06-12 10:54:28 +0300

    Component divided.

---
+++ src/Filter.tsx
@@ -0,0 +1,52 @@
+import {Column} from "@tanstack/react-table";
+import React from "react";
+import DebouncedInput from "./DebouncedInput";
+
+export default function Filter({column}: { column: Column<any> }) {
+    const columnFilterValue = column.getFilterValue()
+    const {filterVariant} = column.columnDef.meta ?? {}
+
+    return filterVariant === 'range' ? (
+        <div>
+            <div className="flex space-x-2">
+                <DebouncedInput
+                    type="number"
+                    value={(columnFilterValue as [number, number])?.[0] ?? ''}
+                    onChange={value =>
+                        column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+                    }
+                    placeholder={`Min`}
+                    className="w-24 border shadow rounded"
+                />
+                <DebouncedInput
+                    type="number"
+                    value={(columnFilterValue as [number, number])?.[1] ?? ''}
+                    onChange={value =>
+                        column.setFilterValue((old: [number, number]) => [old?.[0], value])
+                    }
+                    placeholder={`Max`}
+                    className="w-24 border shadow rounded"
+                />
+            </div>
+            <div className="h-1"/>
+        </div>
+    ) : filterVariant === 'select' ? (
+        <select
+            onChange={e => column.setFilterValue(e.target.value)}
+            value={columnFilterValue?.toString()}
+        >
+            <option value="">All</option>
+            <option value="complicated">complicated</option>
+            <option value="relationship">relationship</option>
+            <option value="single">single</option>
+        </select>
+    ) : (
+        <DebouncedInput
+            className="w-36 border shadow rounded"
+            onChange={value => column.setFilterValue(value)}
+            placeholder={`Search...`}
+            type="text"
+            value={(columnFilterValue ?? '') as string}
+        />
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import {
 } from '@tanstack/react-table'
 import {useVirtualizer} from '@tanstack/react-virtual'
 import {makeData, Person} from './makeData'
+import Filter from "./Filter";
 
 declare module '@tanstack/react-table' {
     interface ColumnMeta<TData extends RowData, TValue> {
@@ -111,6 +112,7 @@ function App() {
                         filterArray.push(
                             <div key={header.id}>
                                 <span>{columns[header.index].header?.toString()}: </span>
+                                {/* eslint-disable-next-line react/jsx-no-undef */}
                                 <Filter column={header.column}/>
                             </div>
                         );
@@ -227,85 +229,6 @@ function App() {
                 2
             )}
         </div>
-    )
-}
-
-function Filter({column}: { column: Column<any> }) {
-    const columnFilterValue = column.getFilterValue()
-    const {filterVariant} = column.columnDef.meta ?? {}
-
-    return filterVariant === 'range' ? (
-        <div>
-            <div className="flex space-x-2">
-                <DebouncedInput
-                    type="number"
-                    value={(columnFilterValue as [number, number])?.[0] ?? ''}
-                    onChange={value =>
-                        column.setFilterValue((old: [number, number]) => [value, old?.[1]])
-                    }
-                    placeholder={`Min`}
-                    className="w-24 border shadow rounded"
-                />
-                <DebouncedInput
-                    type="number"
-                    value={(columnFilterValue as [number, number])?.[1] ?? ''}
-                    onChange={value =>
-                        column.setFilterValue((old: [number, number]) => [old?.[0], value])
-                    }
-                    placeholder={`Max`}
-                    className="w-24 border shadow rounded"
-                />
-            </div>
-            <div className="h-1"/>
-        </div>
-    ) : filterVariant === 'select' ? (
-        <select
-            onChange={e => column.setFilterValue(e.target.value)}
-            value={columnFilterValue?.toString()}
-        >
-            <option value="">All</option>
-            <option value="complicated">complicated</option>
-            <option value="relationship">relationship</option>
-            <option value="single">single</option>
-        </select>
-    ) : (
-        <DebouncedInput
-            className="w-36 border shadow rounded"
-            onChange={value => column.setFilterValue(value)}
-            placeholder={`Search...`}
-            type="text"
-            value={(columnFilterValue ?? '') as string}
-        />
-    )
-}
-
-// A typical debounced input react component
-function DebouncedInput({
-                            value: initialValue,
-                            onChange,
-                            debounce = 500,
-                            ...props
-                        }: {
-    value: string | number
-    onChange: (value: string | number) => void
-    debounce?: number
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
-    const [value, setValue] = React.useState(initialValue)
-
-    React.useEffect(() => {
-        setValue(initialValue)
-    }, [initialValue])
-
-    React.useEffect(() => {
-        const timeout = setTimeout(() => {
-            onChange(value)
-        }, debounce)
-
-        return () => clearTimeout(timeout)
-    }, [value])
-
-    return (
-        <input {...props} value={value} onChange={e => setValue(e.target.value)}/>
     )
 }
 
